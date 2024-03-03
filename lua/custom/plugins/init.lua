0109253b323b63c83a2e577caca56dacd0e90e9d
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-03-03 19:39:16 -0400

    init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,35 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    -- @type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "SS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash")treesitter_search() end, desc = "Treesitter Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
+  -- nvim v0.8.0
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+}
