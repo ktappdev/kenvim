0c5060a7fbaf85f66ca429c549355a2b2e36e4d3
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-03-03 20:07:03 -0400

    no message

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -32,4 +32,14 @@ return {
       'nvim-lua/plenary.nvim',
     },
   },
+  {
+    'Exafunction/codeium.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      require('codeium').setup {}
+    end,
+  },
 }
