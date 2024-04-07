ceb9091cebe3463de11ec9757b490e8ba82f7cd5
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-04-07 08:42:57 -0400

    comment works as expected

--- init.lua
+++ init.lua
@@ -143,19 +143,6 @@ require('lazy').setup {
   --  This is equivalent to:
   --    require('Comment').setup({})
 
-  -- "gc" to comment visual regions/lines
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-      -- add any options here
-    },
-    lazy = false,
-  },
-  {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    event = 'BufRead',
-  },
-
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
   --    require('gitsigns').setup({ ... })
