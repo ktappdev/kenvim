bf8c84592fd263dbfbd5f88bc90b800ed44b0ff0
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-04-08 18:19:13 -0400

    added kenvim header

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -1,9 +1,69 @@
 return {
-  {
-    'startup-nvim/startup.nvim',
-    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('startup').setup { theme = 'startify' }
-    end,
-  },
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		local time = os.date("%H:%M")
+		local date = os.date("%a %d %b")
+		local v = vim.version()
+		local version = " v" .. v.major .. "." .. v.minor .. "." .. v.patch
+
+		-- Set header
+		dashboard.section.header.val =
+
+		[[
+       ,--.                   ,--.                              ____
+   ,--/  /|    ,---,.       ,--.'|               ,---,        ,'  , `.
+,---,': / '  ,'  .' |   ,--,:  : |       ,---.,`--.' |     ,-+-,.' _ |
+:   : '/ / ,---.'   |,`--.'`|  ' :      /__./||   :  :  ,-+-. ;   , ||
+|   '   ,  |   |   .'|   :  :  | | ,---.;  ; |:   |  ' ,--.'|'   |  ;|
+'   |  /   :   :  |-,:   |   \ | :/___/ \  | ||   :  ||   |  ,', |  ':
+|   ;  ;   :   |  ;/||   : '  '; |\   ;  \ ' |'   '  ;|   | /  | |  ||
+:   '   \  |   :   .''   ' ;.    ; \   \  \: ||   |  |'   | :  | :  |,
+|   |    ' |   |  |-,|   | | \   |  ;   \  ' .'   :  ;;   . |  ; |--'
+'   : |.  \'   :  ;/|'   : |  ; .'   \   \   '|   |  '|   : |  | ,
+|   | '_\.'|   |    \|   | '`--'      \   `  ;'   :  ||   : '  |/
+'   : |    |   :   .''   : |           :   \ |;   |.' ;   | |`-'
+;   |,'    |   | ,'  ;   |.'            '---" '---'   |   ;/
+'---'      `----'    '---'                            '---'
+]]
+
+
+
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "   New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
+			dashboard.button("e", "   File Explorer", ":cd $HOME | Neotree<CR>"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("c", "   Configuration", ":e ~/.config/kenvim/init.lua<CR>"),
+			dashboard.button("R", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
+			dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
+		}
+
+		function centerText(text, width)
+			local totalPadding = width - #text
+			local leftPadding = math.floor(totalPadding / 2)
+			local rightPadding = totalPadding - leftPadding
+			return string.rep(" ", leftPadding) .. text .. string.rep(" ", rightPadding)
+		end
+
+		dashboard.section.footer.val = {
+			centerText("Make it with Kenvim: Simplicity, Power, and Efficiency", 50),
+			" ",
+			-- centerText("NvimOnMy_Way❤️", 50),
+			-- " ",
+			centerText(date, 50),
+			centerText(time, 50),
+			centerText(version, 50),
+		}
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
